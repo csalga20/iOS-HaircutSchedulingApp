79d9495bbe03c0ca689ac48cd10c1c2478015c0c
Charlie Salgado <csalga20@depaul.edu>
2022-10-07 21:48:55 -0500

    Initial Commit

---
+++ HaircutSchedulingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaircutSchedulingApp
+//
+//  Created by Charles Salgado on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
